8a2873872b331a565343dcfc5863dd1dd2d378a4
Ben Jones <benpaddlejones@outlook.com>
2023-02-20 12:45:57 +1100

    update script

--- Assets/Scripts/LearnVariables.cs
+++ Assets/Scripts/LearnVariables.cs
@@ -7,11 +7,33 @@ using TMPro;
 
 public class LearnVariables : MonoBehaviour
 {
+    /* Types of Variables
+    public string PlayerName = "Mr Jones";
+    public int MrJonesInt = 1;
+    public bool MrJonesBooleen = false;
+    public float MrjonesFloat = 0.001;
+    public double MrJonesDouble = 0.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001
+    public char MrJones = 'c'; */
+
+    // Basic Maths
+    public int IntOne = 1;
+    public int IntTwo = 2;
+    private int IntMath = 0;
+    public string StringOne = "The Answer is: ";
+    public string StringTwo = "???";
+    private bool BoolOne = false;
 
     public TextMeshProUGUI ComputerFeedbackObject;
 
     void Update()
     {
-        ComputerFeedbackObject.text = " ";
+        IntMath = IntOne * IntTwo;
+        if (IntMath >= 10)
+            {
+                ComputerFeedbackObject.text = StringOne + IntMath.ToString();
+            } else 
+            {
+                ComputerFeedbackObject.text = StringTwo;
+            }
     }
 }

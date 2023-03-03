4c952f7120e9776971cbbcfa4ff0437c222462b2
Ben Jones <benpaddlejones@outlook.com>
2023-03-03 16:26:53 +1100

    AddDoandDoWhile

---
+++ Assets/Scripts/DoWhileandWhile.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class DoWhileandWhile : MonoBehaviour
+{
+
+    private int GameOver = 1;
+
+    public TextMeshProUGUI OutputObject;
+
+    public GameObject[] ColoredSpheres;
+    private Color[] ColorArray = { Color.red, Color.blue, Color.green, Color.yellow};
+
+    void Update ()
+    {
+
+        if (Input.GetKeyDown("space"))
+        {
+            GameOver = 0;
+        } else {
+            GameOver = 1;
+        }
+
+        while (GameOver < 1)
+        {
+            GameOver++;
+            ChangeColours();
+        } 
+    }
+
+    void ChangeColours ()
+    {
+        for (int SphereIndex = 0; SphereIndex < ColoredSpheres.Length; SphereIndex++)
+        {
+            int RandomColor = Random.Range(0, ColoredSpheres.Length +1);
+            ColoredSpheres[SphereIndex].GetComponent<MeshRenderer>().material.color = ColorArray[RandomColor];
+        }
+    }
+
+}
